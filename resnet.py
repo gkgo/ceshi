d7fad486bdc159cca88f684e055de896c936dcab
gkgo <91817417+gkgo@users.noreply.github.com>
2023-02-28 11:06:48 +0800

    Update resnet.py

--- resnet.py
+++ resnet.py
@@ -343,7 +343,7 @@ class ConvNet4(nn.Module):
 #         x = x.mean(dim=[-1, -2])
 #         x = self.fc(x)
 
-        x = self.avgpool(x1)
+        x = self.avgpool(x)
         x = x.view(x.size(0), -1)
         x = self.fc(x)
 
