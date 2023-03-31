3232666c58eeb1703074f791c28af30e95098bbf
gkgo <91817417+gkgo@users.noreply.github.com>
2023-03-31 19:20:35 +0800

    Update train.py

--- train.py
+++ train.py
@@ -58,7 +58,7 @@ def main():
     device = torch.device("cuda" if GPU else "cpu")
 
 
-    train_data = torchvision.datasets.CIFAR10('./',
+    train_data = torchvision.datasets.CIFAR100('./',
                                                transform=transforms.Compose([
         transforms.RandomCrop(32, padding=4),
         transforms.RandomHorizontalFlip(),
@@ -66,7 +66,7 @@ def main():
         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
     ]), download=True)
     train_loader = torch.utils.data.DataLoader(train_data, batch_size=128, shuffle=True, num_workers=0)
-    val_data = torchvision.datasets.CIFAR10('./', transform=transforms.Compose([
+    val_data = torchvision.datasets.CIFAR100('./', transform=transforms.Compose([
         transforms.ToTensor(),
         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
     ]), train=False, download=True)
