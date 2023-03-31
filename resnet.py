005dc8bee8f8038a5c26260277e2580a99cc378d
gkgo <91817417+gkgo@users.noreply.github.com>
2023-03-31 11:32:40 +0800

    Update resnet.py

--- resnet.py
+++ resnet.py
@@ -2,33 +2,44 @@ import torch.nn.functional as F
 import torch
 import torch.nn as nn
 
+
+def conv3x3(in_planes, out_planes, stride=1):
+    """3x3 convolution with padding"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+                     padding=1, bias=False)
+
+def gaussian_normalize( x, dim, eps=1e-05):
+    x_mean = torch.mean(x, dim=dim, keepdim=True)
+    x_var = torch.var(x, dim=dim, keepdim=True)  # 求dim上的方差
+    x = torch.div(x - x_mean, torch.sqrt(x_var + eps))  # （x原始-x平均）/根号下x_var
+    return x
+
 def featureL2Norm(feature):
     epsilon = 1e-6
     norm = torch.pow(torch.sum(torch.pow(feature, 2), 1) + epsilon, 0.5).unsqueeze(1).expand_as(feature)
     return torch.div(feature, norm)
 
 class mySelfCorrelationComputation(nn.Module):
-    def __init__(self, kernel_size=(1, 1), padding=0):
+    def __init__(self,channel, kernel_size=(1, 1), padding=0):
         super(mySelfCorrelationComputation, self).__init__()
-        planes =[640, 64, 64, 640]
         self.kernel_size = kernel_size
         self.unfold = nn.Unfold(kernel_size=kernel_size, padding=padding)
         self.relu = nn.ReLU(inplace=False)
 
-        self.conv1x1_in = nn.Sequential(nn.Conv2d(planes[0], planes[1], kernel_size=1, bias=False, padding=0),
-                                        nn.BatchNorm2d(planes[1]),
+        self.conv1x1_in = nn.Sequential(nn.Conv2d(channel, channel, kernel_size=1, bias=False, padding=0),
+                                        nn.BatchNorm2d(channel),
                                         nn.ReLU(inplace=True))
-        self.embeddingFea = nn.Sequential(nn.Conv2d(1280, 640,
+        self.embeddingFea = nn.Sequential(nn.Conv2d(channel*2, channel,
                                                      kernel_size=1, bias=False, padding=0),
-                                           nn.BatchNorm2d(640),
+                                           nn.BatchNorm2d(channel),
                                            nn.ReLU(inplace=True))
         self.conv1x1_out = nn.Sequential(
-            nn.Conv2d(640, 640, kernel_size=1, bias=False, padding=0),
-            nn.BatchNorm2d(640))
+            nn.Conv2d(channel, channel, kernel_size=1, bias=False, padding=0),
+            nn.BatchNorm2d(channel))
 
     def forward(self, x):
 
-#         x = self.conv1x1_in(x)
+        # x = self.conv1x1_in(x)
         b, c, h, w = x.shape
 
         x0 = self.relu(x)
@@ -53,54 +64,8 @@ class mySelfCorrelationComputation(nn.Module):
         # embed
         feature_embd = self.embeddingFea(feature_cat)
         feature_embd = self.conv1x1_out(feature_embd)
+        feature_embd = self.relu(feature_embd)
         return feature_embd
-
-
-
-
-class SqueezeExcitation(nn.Module):
-    def __init__(self, channel, reduction=16):
-        super(SqueezeExcitation, self).__init__()
-        hdim = 64
-        self.conv1x1_in = nn.Sequential(nn.Conv2d(channel, hdim, kernel_size=1, bias=False, padding=0),
-                                        nn.BatchNorm2d(hdim),
-                                        nn.ReLU(inplace=False))
-        self.conv1x1_out = nn.Sequential(nn.Conv2d(hdim, channel, kernel_size=1, bias=False, padding=0),
-                                        nn.BatchNorm2d(channel),
-                                        nn.ReLU(inplace=False))
-        self.avg_pool = nn.AdaptiveAvgPool2d(1)
-        self.fc = nn.Sequential(
-            nn.Linear(hdim, hdim // reduction, bias=False),
-            nn.ReLU(inplace=False),
-            nn.Linear(hdim // reduction, hdim, bias=False),
-            nn.Sigmoid()
-        )
-
-    def forward(self, x):
-        x = self.conv1x1_in(x)
-        b, c, _, _ = x.size()
-        y = self.avg_pool(x).view(b, c)
-        y = self.fc(y).view(b, c, 1, 1)
-        x = x * y.expand_as(x)
-        x = self.conv1x1_out(x)
-        return x
-
-
-
-
-
-def conv3x3(in_planes, out_planes, stride=1):
-    """3x3 convolution with padding"""
-    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
-                     padding=1, bias=False)
-
-def gaussian_normalize( x, dim, eps=1e-05):
-    x_mean = torch.mean(x, dim=dim, keepdim=True)
-    x_var = torch.var(x, dim=dim, keepdim=True)  # 求dim上的方差
-    x = torch.div(x - x_mean, torch.sqrt(x_var + eps))  # （x原始-x平均）/根号下x_var
-    return x
-
-
 
 class BasicBlock(nn.Module):
     expansion = 1
@@ -144,7 +109,7 @@ def normalize_feature(x):
 
 class ResNet(nn.Module):
 
-    def __init__(self, block, num_classes=100, zero_init_residual=False):
+    def __init__(self, block, num_classes=10, zero_init_residual=False):
         self.inplanes = 3
         super(ResNet, self).__init__()
 
@@ -152,10 +117,20 @@ class ResNet(nn.Module):
         self.layer2 = self._make_layer(block, 160, stride=2)
         self.layer3 = self._make_layer(block, 320, stride=2)
         self.layer4 = self._make_layer(block, 640, stride=2)
-        self.scr_module = mySelfCorrelationComputation(kernel_size=(1,1), padding=0)
         # self.scr_module = SqueezeExcitation(channel=640)
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
         self.fc = nn.Linear(640, num_classes)
+        self.scr_module0 = mySelfCorrelationComputation(channel=64,kernel_size=(1, 1), padding=0)
+        self.scr_module1 = mySelfCorrelationComputation(channel=128, kernel_size=(1, 1), padding=0)
+        self.scr_module2 = mySelfCorrelationComputation(channel=256, kernel_size=(1, 1), padding=0)
+        self.scr_module = mySelfCorrelationComputation(channel=640,kernel_size=(1, 1), padding=0)
+        self.relu = nn.LeakyReLU(0.1)
+        self.maxpool = nn.MaxPool2d(1)
+        # self.scr_module = cbam_block(channel=640)
+        # self.scr_module = SqueezeExcitation(channel=640)
+        self.conv1x1_out = nn.Sequential(
+            nn.Conv2d(1024, 640, kernel_size=1, bias=False, padding=0),
+            nn.BatchNorm2d(640))
 
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
@@ -180,113 +155,47 @@ class ResNet(nn.Module):
         return nn.Sequential(*layers)
 
     def forward(self, x):
-        x = self.layer1(x)
 
-        x = self.layer2(x)
+        out1 = self.layer1(x)
+        out1_s = self.scr_module0(out1)
+        out1 = out1 + out1_s
+        out1 = self.relu(out1)
 
-        x = self.layer3(x)
+        out2 = self.layer2(out1)
+        out2_s = self.scr_module1(out2)
+        out2 = out2 + out2_s
+        out2 = self.relu(out2)
 
-        x = self.layer4(x)
-#____________________________________
-        identity = x
+        out3 = self.layer3(out2)
+        out3_s = self.scr_module2(out3)
+        out3 = out3 + out3_s
+        out3 = self.relu(out3)
 
-        x = self.scr_module(x)
+        out4 = self.layer4(out3)
+        out4_s = self.scr_module(out4)
+        out4 = out4 + out4_s
+        out4 = self.relu(out4)
 
+        # ___________________________________________________________
+        out2 = F.max_pool2d(out2, out2.size()[2:])
+        out3 = F.max_pool2d(out3, out3.size()[2:])
+        out4 = F.max_pool2d(out4, out4.size()[2:])
 
-        x = x + identity
+        out2 = F.layer_norm(out2, out2.size()[1:])
+        out3 = F.layer_norm(out3, out3.size()[1:])
+        out4 = F.layer_norm(out4, out4.size()[1:])
 
-        x = F.relu(x, inplace=True)
+        out = torch.cat([out4, out3, out2], 1)
+        out = self.conv1x1_out(out)
+        out = self.relu(out)
+        out = self.maxpool(out)
 
-        b, c, h, w = x.shape
-        x = normalize_feature(x)
-
-        y = F.normalize(x, p=2, dim=1, eps=1e-8)
-
-        d_s = y.view(b, c, -1)
-        d_s = gaussian_normalize(d_s, dim=2)
-
-        d_s = F.softmax(d_s /2, dim=2)
-        d_s = d_s.view(b,c,h, w)
-
-        x1 = d_s + x
-
-        x = x1.mean(dim=[-1, -2])
+        # __________________________________________
+        x = self.avgpool(out)
+        x = x.view(x.size(0), -1)
         x = self.fc(x)
-#_______________________________________________________
-        # identity = x
-        # x = self.avgpool(x)
-        # x = x.view(x.size(0), -1)
-        # x = self.fc(x)
-        #
-        #
-        # # Self-correlation module
-        # feat_corr = self.scr_module(identity)
-        # feat_corr = feat_corr.mean([2, 3])
-        # feat_corr = self.fc(feat_corr)
-        # out = x + feat_corr
 
         return x
 
 def resnet12():
     return ResNet(BasicBlock)
-
-
-
-
-
-def conv_block(in_channels, out_channels):
-    return nn.Sequential(
-        nn.Conv2d(in_channels, out_channels, 3, padding=1),
-        nn.BatchNorm2d(out_channels),
-        nn.ReLU(),
-        nn.MaxPool2d(2)
-    )
-
-class ConvNet4(nn.Module):
-
-    def __init__(self, num_classes=100, x_dim=3, hid_dim=64, z_dim=640):
-        super().__init__()
-        self.encoder = nn.Sequential(
-            conv_block(x_dim, hid_dim),
-            conv_block(hid_dim, hid_dim),
-            conv_block(hid_dim, hid_dim),
-            conv_block(hid_dim, z_dim),
-        )
-        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-        self.fc = nn.Linear(640, num_classes)
-#         self.scr_module = mySelfCorrelationComputation(kernel_size=(1, 1), padding=0)
-        self.scr_module = SqueezeExcitation(channel=640)
-
-    def forward(self, x):
-        x = self.encoder(x)
-
-        identity = x
-
-        x = self.scr_module(x)
-
-
-        x = x + identity
-
-        x = F.relu(x, inplace=True)
-       
-#         b, c, h, w = x.shape
-#         x = normalize_feature(x)
-
-#         y = F.normalize(x, p=2, dim=1, eps=1e-8)
-
-#         d_s = y.view(b, c, -1)
-#         d_s = gaussian_normalize(d_s, dim=2)
-
-#         d_s = F.softmax(d_s /2, dim=2)
-#         d_s = d_s.view(b,c,h, w)
-
-#         x1 = d_s + x
-
-#         x = x1.mean(dim=[-1, -2])
-#         x = self.fc(x)
-
-        x = self.avgpool(x)
-        x = x.view(x.size(0), -1)
-        x = self.fc(x)
-
-        return x
